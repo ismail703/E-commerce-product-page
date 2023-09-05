ecf9056eacf9873e6026fc75bf2448fb6bca42a5
Ismail Elmain <114304748+ismail703@users.noreply.github.com>
2023-09-05 08:49:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
+// ************** Header **************
 const closeBtn = document.querySelector(".close-btn");
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector("nav");
 const cartIcon = document.querySelector(".cart-icon");
 const cartContent = document.querySelector(".cart-content");
 const overlay = document.querySelector(".overlay");
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
-const slideImg = document.querySelectorAll(".slider-image img");
-
-slideImg.forEach((img, index) => {
-    img.style.left = `${index * 100}%`;
-});
 
 menu.addEventListener("click", function() {
     navbar.classList.add("show-navbar");
@@ -26,6 +20,18 @@ cartIcon.addEventListener('click', function () {
     cartContent.classList.toggle("show-cart");
 });
 
+// ************** Product image **************
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slideImg = document.querySelectorAll(".slider-image img");
+const mainImg = document.querySelector(".main");
+const secondaryImgs = document.querySelectorAll(".secondary");
+
+
+slideImg.forEach((img, index) => {
+    img.style.left = `${index * 100}%`;
+});
+
 let counter = 0;
 
 nextBtn.addEventListener('click', function () {
@@ -38,7 +44,20 @@ prevBtn.addEventListener('click', function () {
     carousel();
 });
 
+secondaryImgs.forEach((img, index)=> {
+    img.addEventListener('click', (e) => {
+        secondaryImgs.forEach(img => {
+            img.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+        mainImg.src = `images/image-product-${index+1}.jpg`;
+    });
+})
+// ************** Functions **************
 function carousel() {
+    // back to default image
+    // mainImg.src = `images/image-product-1.jpg`;
+
     if (counter === slideImg.length) {
         counter = 0;
     }
